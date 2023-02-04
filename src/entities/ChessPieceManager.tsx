34dcb8cfa23f8abeee733cb47b741e95b47a240f
tancheanyong <tancheanyong@gmail.com>
2023-02-04 14:19:28 +0800

    feat: Pawn

--- src/entities/ChessPieceManager.tsx
+++ src/entities/ChessPieceManager.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import PawnPiece from "./chess_pieces/PawnPiece";
 import { QueenPiece } from "./chess_pieces/QueenPiece";
 
 enum ChessPieceNames {
+  PAWN = "pawn",
   QUEEN = "queen",
 }
 
@@ -22,9 +24,18 @@ const CHESS_PIECES: ChessPieceTypes[] = [
   {
     name: ChessPieceNames.QUEEN,
     color: ChessPieceColors.BLACK,
-    position: [0.3, 1, 0.3],
+    position: [0.2, 1, 0.2],
   },
-  { name: ChessPieceNames.QUEEN, color: ChessPieceColors.WHITE },
+  {
+    name: ChessPieceNames.QUEEN,
+    color: ChessPieceColors.WHITE,
+    position: [0.4, 1, 0.4],
+  },
+  {
+    name: ChessPieceNames.PAWN,
+    color: ChessPieceColors.WHITE,
+    position: [-0.1, 1, -0.1],
+  },
 ];
 
 const ChessPieceManager = () => {
@@ -35,6 +46,8 @@ const ChessPieceManager = () => {
         switch (name) {
           case ChessPieceNames.QUEEN:
             return <QueenPiece color={color} position={position} />;
+          case ChessPieceNames.PAWN:
+            return <PawnPiece color={color} position={position} />;
           default:
             return null;
         }
